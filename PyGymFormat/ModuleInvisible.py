51a32305e2023dc9fa6d7e87bc332134302bc616
Jeremy <jeremytaylorneumann@gmail.com>
2024-03-21 12:32:12 +0100

    made repository

--- PyGymFormat/ModuleInvisible.py
+++ PyGymFormat/ModuleInvisible.py
@@ -102,30 +102,3 @@ class Hitbox(ABC):
         raise NotImplementedError
 
 
-class Physics(ABC):
-    _rules: List[Union[Callable, Any]]
-
-    @property
-    def rules(self):
-        return self._rules
-
-    @rules.setter
-    def rules(self, rules: List[Union[Callable, Any]]):
-        self._rules = rules
-
-    @rules.deleter
-    def rules(self):
-        self._rules = []
-
-    def append(self, rule: List[Union[Callable, Any]]):
-        self._rules.append(rule)
-
-    def modify(self, index: int, new_rule: Union[Callable, Any]):
-        self._rules[index] = new_rule
-
-    def extend(self, rules: List[Union[Callable, Any]]):
-        self._rules.extend(rules)
-
-    @abstractmethod
-    def step(self, dt):
-        raise NotImplementedError
