6fb650386fe6eb9cd40bb59d70a1247419bcaf96
Jeremy <jeremytaylorneumann@gmail.com>
2024-03-21 15:22:46 +0100

    changed name, added player modules

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PyGymFormat",
+    name="pygymformat",
     version="0.0.1",
     packages=find_packages(),
     license="MIT",
-    description="Base classes for making games that comply with Openai Gym enviroment creation standards",
+    description="Base classes for making games that comply with Openai Gym environment creation standards",
     long_description=open("README.md").read(),
     url="https://github.com/Bassneptun/PyGymFormat",
     install_requires=["pygame", "numpy", "vectormath"],
